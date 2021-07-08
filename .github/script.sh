5b2b71ab12a75392ff0ff5d7ef5689d38189d9f6
Abada Samuel Oghenero <abadasamuelosp@gmail.com>
2021-07-08 20:37:17 +0100

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -6,7 +6,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
 find lib/ -name *.dart -print0 | while read -d $'\0' file
 do
         name="$(basename ${file})"
-        grep -rn -F -q "${name}" lib/
+        grep -rn -F -q "${name}" /home/runner/work/bloom/bloom/lib/
         if [ $? -ne 0 ]
         then
                 echo "Unused file: ${file}"
